32ccc833063c48e7aae27f783970c38743b60aa1
amo102 <a_modahl@hotmail.com>
2017-03-05 16:01:22 +0100

    Propagate fetch function to chessgame

--- src/containers/Chess.js
+++ src/containers/Chess.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux'
 import { toggleReadMore } from '../actions/Chess'
+import { myFetch } from '../actions/MyFetch'
 import Chess from '../components/Chess'
 
 const ChessContainer = ((Target, namespace) => {
@@ -8,16 +9,26 @@ const ChessContainer = ((Target, namespace) => {
       toggleReadMore: (readMore) => {
         let action = toggleReadMore(readMore)
         dispatch(action)
-      }
+      },
+      dispatch: dispatch
     }
   }
 
   const mapStateToProps = (state) => {
     const localState = namespace ? state[namespace] : state
-    return {
-      readMore: localState.readMore,
-      token: localState.token
+
+    let props = {
+      readMore: localState.readMore
     }
+
+    if (localState.jwToken && localState.jwToken !== null) {
+      props = {
+        myFetch: (dispatch) => myFetch(localState.jwToken, dispatch),
+        ...props
+      }
+    }
+
+    return props
   }
 
   return connect(mapStateToProps, mapDispatchToProps)(Target)
