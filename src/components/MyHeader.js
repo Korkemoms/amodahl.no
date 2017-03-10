545beae2911bc9dadcb8998c15732bfab87b05aa
amo102 <a_modahl@hotmail.com>
2017-03-10 22:31:55 +0100

    More backgrounds

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 
-const MyHeader = ({ headline }) => {
+const MyHeader = ({ headline, backgroundClass }) => {
   return (
     <div className='bs-docs-header Header'>
       <div className='container'>
@@ -9,13 +9,14 @@ const MyHeader = ({ headline }) => {
           <h1>{headline}</h1>
         </div>
       </div>
-      <div className='Background-container' />
+      <div className={'Background-container ' + backgroundClass} />
     </div>
   )
 }
 
 MyHeader.propTypes = {
-  headline: PropTypes.string.isRequired
+  headline: PropTypes.string.isRequired,
+  backgroundClass: PropTypes.string
 }
 
 export default MyHeader
