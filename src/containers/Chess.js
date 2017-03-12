986fd5686e6f2f933161f10685efddfd3a758979
amo102 <a_modahl@hotmail.com>
2017-03-12 19:59:36 +0100

    Propagate navigate method

--- src/containers/Chess.js
+++ src/containers/Chess.js
@@ -2,21 +2,23 @@ import { connect } from 'react-redux'
 import { toggleReadMore } from '../actions/Chess'
 import { myFetch } from '../actions/MyFetch'
 import Chess from '../components/Chess'
+import { push } from 'react-router-redux'
 
 const ChessContainer = ((Target, namespace) => {
   const mapDispatchToProps = (dispatch) => {
     return {
       toggleReadMore: (readMore) => {
-        let action = toggleReadMore(readMore)
-        dispatch(action)
+        dispatch(toggleReadMore(readMore))
       },
-      myFetch: myFetch(dispatch)
+      myFetch: myFetch(dispatch),
+      navigate: (path) => {
+        dispatch(push(path))
+      }
     }
   }
 
   const mapStateToProps = (state) => {
     const localState = namespace ? state[namespace] : state
-
     let props = {
       readMore: localState.readMore,
       myEmail: localState.myEmail,
