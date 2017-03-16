b857c1917d2c1c979b47001ba4b3713a22e63076
amo102 <a_modahl@hotmail.com>
2017-03-17 00:28:52 +0100

    Removed redundant div

--- src/components/MatrixMultiplication.js
+++ src/components/MatrixMultiplication.js
@@ -18,42 +18,38 @@ const MatrixMultiplication = () => {
     <DocumentTitle title='Matrix multiplication game'>
       <div>
         <MyHeader headline='Matrix multiplication game' />
-
         <div style={{background: 'white'}}>
-
-          <div style={{background: 'white'}}>
-            <Grid>
-              <Row>
-                <Col className='mycontent' xs={12} sm={8}>
-                  <h1>About this game</h1>
-                  <p>This is a small game for Android where players practice multiplying matrices.</p>
-                  <p>
-              My own experience:<br />
-              I use the app to compute just a few matrices a week, when I am in an
-              elevator or waiting for the bus. It has improved the speed of my mental
-              arithmetic a lot, I also think and reason about matrices and vectors
-              much quicker than before.
-            </p>
-                </Col>
-                <Col xs={12} sm={4}>
-                  <Image className='myimage' src={GameImage} responsive />
-                </Col>
-              </Row>
-              <Row>
-                <Col xs={12}>
-                  <p>
-                    <label>Play store:</label>{' '}
-                    <a href='https://play.google.com/store/apps/details?id=org.ajm.laforkids'>
-                      {'https://play.google.com/store/apps/details?id=org.ajm.laforkids'}</a>
-                    <br />
-                    <label>Source code:</label>{' '}
-                    <a href='https://github.com/Korkemoms/Matrix-Multiplication'>
-                      {'https://github.com/Korkemoms/Matrix-Multiplication'}</a>
-                  </p>
-                </Col>
-              </Row>
-            </Grid>
-          </div>
+          <Grid>
+            <Row>
+              <Col className='mycontent' xs={12} sm={8}>
+                <h1>About this game</h1>
+                <p>This is a small game for Android where players practice multiplying matrices.</p>
+                <p>
+                  My own experience:<br />
+                  I use the app to compute just a few matrices a week, when I am in an
+                  elevator or waiting for the bus. It has improved the speed of my mental
+                  arithmetic a lot, I also think and reason about matrices and vectors
+                  much quicker than before.
+                </p>
+              </Col>
+              <Col xs={12} sm={4}>
+                <Image className='myimage' src={GameImage} responsive />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12}>
+                <p>
+                  <label>Play store:</label>{' '}
+                  <a href='https://play.google.com/store/apps/details?id=org.ajm.laforkids'>
+                    {'https://play.google.com/store/apps/details?id=org.ajm.laforkids'}</a>
+                  <br />
+                  <label>Source code:</label>{' '}
+                  <a href='https://github.com/Korkemoms/Matrix-Multiplication'>
+                    {'https://github.com/Korkemoms/Matrix-Multiplication'}</a>
+                </p>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </div>
     </DocumentTitle>
