4a304f1222656045d1144f8486d335967c50a45e
amo102 <a_modahl@hotmail.com>
2017-03-17 01:33:48 +0100

    New page

--- src/reducers/LeveringMontering.js
+++ src/reducers/LeveringMontering.js
@@ -5,13 +5,10 @@ const initialState = {
 
 export default function update (state = initialState, action) {
   switch (action.type) {
-    case 'DISABLE_FADE_IN':
+    case '@@leveringMontering/CAROUSEL_SELECT':
       return Object.assign({}, state, {
-        fadeInEnabled: false
-      })
-    case 'ENABLE_FADE_IN':
-      return Object.assign({}, state, {
-        fadeInEnabled: true
+        carouselIndex: action.selectedIndex,
+        carouselDirection: action.direction
       })
     default:
       return state
