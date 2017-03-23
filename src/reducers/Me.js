99ecd6bc54054a70ef213e943aa225d5872d6c3e
amo102 <a_modahl@hotmail.com>
2017-03-23 02:27:59 +0100

    Changed some actions

--- src/reducers/Me.js
+++ src/reducers/Me.js
@@ -8,35 +8,28 @@ const initialState = {
 export default function update (state = initialState, action) {
   switch (action.type) {
 
-    case 'RECEIVE_TOKEN_FROM_AMODAHL':
+    case 'RECEIVE_AMODAHL_TOKEN':
       return Object.assign({}, state, {
         token: action.token,
         name: action.name,
         email: action.email,
         message: null
       })
-    case 'RECEIVE_TOKEN_FROM_LOCAL_STORAGE':
-      return Object.assign({}, state, {
-        token: action.token,
-        name: action.name,
-        email: action.email,
-        message: null
-      })
-    case 'REQUEST_TOKEN_FAILED':
+    case 'REQUEST_AMODAHL_TOKEN_FAILED':
       return Object.assign({}, state, {
         token: null,
         name: null,
         email: null,
         message: null
       })
-    case '@@me/DELETE_TOKEN':
+    case '@@me/DELETE_AMODAHL_TOKEN':
       return Object.assign({}, state, {
         token: null,
         name: null,
         email: null,
         message: 'You logged out.'
       })
-    case '@@login/DELETE_TOKEN':
+    case '@@login/DELETE_AMODAHL_TOKEN':
       return Object.assign({}, state, {
         token: null,
         name: null,
