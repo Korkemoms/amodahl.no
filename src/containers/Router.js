9be398ddfe932217836bc9bfce0d7e5ab785287a
amo102 <a_modahl@hotmail.com>
2017-03-28 02:47:59 +0200

    added anchor scroll support

--- src/containers/Router.js
+++ src/containers/Router.js
@@ -14,15 +14,37 @@ import AmodahlNo from './AmodahlNo'
 import MatrixMultiplication from './MatrixMultiplication'
 import LeveringMontering from './LeveringMontering'
 
+// scroll to the anchor
+function hashLinkScroll (newPage) {
+  const { hash } = window.location
+  if (hash !== '') {
+    const f = () => {
+      const id = hash.replace('#', '')
+      const element = document.getElementById(id)
+      if (element) element.scrollIntoView()
+    }
+    if (newPage) {
+      // Push onto callback queue so it runs after the DOM is updated,
+      // this is required when navigating from a different page so that
+      // the element is rendered on the page before trying to getElementById.
+      setTimeout(() => {
+        f()
+      }, 0)
+    } else {
+      f()
+    }
+  }
+}
+
 /* Keeps url in sync with application */
 export default class MyRouter extends React.Component {
-
   render () {
     let store = this.props.store
     const history = syncHistoryWithStore(browserHistory, store)
 
     return (
-      <Router history={history}>
+      <Router history={history}
+        onUpdate={hashLinkScroll}>
         <Route path='/' component={App}>
           <IndexRoute component={Home} />
           <Route path='/' component={Home} />
